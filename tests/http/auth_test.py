e1ba62a93a5701151a7c6f03757bb582e53aef0e
Joshua Smee <smeejosh81@gmail.com>
2022-04-01 23:51:47 +1100

    Updated test methods

--- tests/http/auth_test.py
+++ tests/http/auth_test.py
@@ -84,6 +84,7 @@ def test_invalid_password_login():
                         json={"email": "email3@email.com", "password": "Password321"})
         assert resp.status_code == 400
 
+
 def test_working_login():
     with test_app.test_client() as app:
         app.post(
@@ -94,16 +95,19 @@ def test_working_login():
         assert resp.status_code == 200
         assert "token" in json.loads(resp.data)
 
+
 '''
             ========================================================
                             auth remove tests
             ========================================================
 '''
 
+
 def test_invalid_token_remove():
     with test_app.test_client() as app:
-        resp = app.post("/auth/remove", headers = {"token", "Wrong token"})
+        resp = app.delete("/auth/remove", headers={"token": "Token is wrong"})
         assert resp.status_code == 403
+
 
 def test_working_remove():
     with test_app.test_client() as app:
@@ -111,15 +115,15 @@ def test_working_remove():
             "/auth/register", json={"email": "email6@email.com", "password": "Password123"})
 
         assert resp.status_code == 200
-        
+
         data = json.loads(resp.data)
-        
-        resp = app.post("/auth/remove", headers = {"token", data['token']})
+
+        resp = app.delete("/auth/remove", headers={"token": data["token"]})
         assert resp.status_code == 200
-        
-        resp = app.post("/auth/remove", headers = {"token", data['token']})
+
+        resp = app.delete("/auth/remove", headers={"token": data["token"]})
         assert resp.status_code == 403
-        
+
         resp = app.post("/auth/login",
                         json={"email": "email6@email.com", "password": "Password321"})
         assert resp.status_code == 400
