2a4b031e49daac1a2558fa26e03d664f4993ca93
Joshua Smee <smeejosh81@gmail.com>
2022-04-02 22:57:10 +1100

    Pylint fixes

--- app/functions/apis.py
+++ app/functions/apis.py
@@ -5,9 +5,10 @@ import json
 from app.models import Session, User, Token, db
 from app.functions.error import AccessError, ServiceUnavailableError
 
+
 def connect(session_id):
     """Gets all required tokens for a user"""
-    
+
     send_token = start_send_session()
     cleanup_tokens(session_id)
 
@@ -17,11 +18,12 @@ def connect(session_id):
 
     return {"send_token": send_token}
 
+
 def disconnect(session_id):
     """Disconnects all tokens for a user"""
     token = Token.query.filter(Token.sessionId == session_id).first()
 
-    if token != None:
+    if token is not None:
         end_send_session(token.send_token)
         cleanup_tokens(session_id)
 
@@ -35,13 +37,16 @@ def start_send_session():
                          'username': os.environ.get("SENDUSERNAME"), 'password': os.environ.get("SENDPASSWORD")})
 
     if resp.status_code != 200:
-        raise ServiceUnavailableError(description= "Cannot reach send email API, please try again later.")
+        raise ServiceUnavailableError(
+            description="Cannot reach send email API, please try again later.")
 
     return json.loads(resp.text)['token']
 
+
 def end_send_session(token):
-    resp = requests.post("https://fudge2021.herokuapp.com/session/end", json={
+    requests.post("https://fudge2021.herokuapp.com/session/end", json={
                          'token': token})
+
 
 def cleanup_tokens(session_id):
     old_tokens = Token.query.filter(Token.sessionId == session_id).all()
--- app/functions/error.py
+++ app/functions/error.py
@@ -1,14 +1,17 @@
 from werkzeug.exceptions import HTTPException
 
+
 class AccessError(HTTPException):
     """403 - HTTP access error"""
     code = 403
     message = 'Tried to access something you do not have permissions for'
 
+
 class InputError(HTTPException):
     """400 - HTTP input error"""
     code = 400
     message = 'Input incorrect'
+
 
 class ServiceUnavailableError(HTTPException):
     """503 - Service Unavailable"""
